2467adeb8a210c3f6bba2edfe2b5f43bea7b3081
Alex Winter <35019706+pyxalweb@users.noreply.github.com>
2023-05-29 18:44:57 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print ('Hello World')
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+print ('Hello World 1')
+
+# Create a new instance of the Chrome driver
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
+
+# Navigate to the URL
+driver.get('https://www.kexp.org/playlist/')
+
+print ('Hello World 2')
